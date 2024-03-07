252925421c0483a52a63b3868b83a03c468f1252
Renan <brufato17.renan@gmail.com>
2024-03-06 21:13:12 -0300

    feat: adicionando handlebars ao projeto

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const exphbs = require('express-handlebars') //objeto do handlebars
 const app = express()
+const path = require('path') //preciso dele para dizer ao handlebars o diretório onde estarão as views
 const db = require('./db/connection')
 const bodyParser = require('body-parser')
 
@@ -11,6 +13,11 @@ app.listen(PORT, function(){
 
 //body parser
 app.use(bodyParser.urlencoded({extended: false}))
+
+//handle bars
+app.set('views', path.join(__dirname,'views')) 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('veiw engine', 'handlebars')
 
 //db connnection
 db
